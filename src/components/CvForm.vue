df92120a4f7c0a5b16c9411cd38b66922367d743
supersakana <zacwilliamson@icloud.com>
2023-04-22 19:38:03 -0600

    moved experience form from CvForm to ExperienceForm component

--- src/components/CvForm.vue
+++ src/components/CvForm.vue
@@ -12,104 +12,47 @@
 
       <!-- Education -->
 
-      <div>
-        <div class="flex items-center justify-between">
-          <h4 className="section-title">Education</h4>
-          <ion-icon name="caret-up" class="cursor-pointer hover:text-gray-600 text-2xl" @click="onFormToggle($event, 'education')"></ion-icon>
-        </div>
-        <EducationForm :contents="contents" 
-                       :toggles="toggles" 
-                       @update-contents="updateContents"  
-                       @delete-form="deleteForm" 
-                       @add-form="addForm" />
+    <div>
+      <div class="flex items-center justify-between">
+        <h4 className="section-title">Education</h4>
+        <ion-icon name="caret-up" class="cursor-pointer hover:text-gray-600 text-2xl" @click="onFormToggle($event, 'education')"></ion-icon>
       </div>
+      <EducationForm :contents="contents" 
+                      :toggles="toggles" 
+                      @update-contents="updateContents"  
+                      @delete-form="deleteForm" 
+                      @add-form="addForm" />
+    </div>
 
       <!-- Experience -->
 
-      <div>
-        <div class="flex items-center justify-between">
-          <h4 className="section-title">Experience</h4>
-          <ion-icon name="caret-up" @click="onFormToggle($event, 'experience')" class="cursor-pointer hover:text-gray-600 text-2xl"></ion-icon>
-        </div>
-
-        <div id="experience-form" v-if="toggles['experience']">
-          <ion-icon name="add-circle" class="text-green-600 text-xl cursor-pointer" @click="onAddForm('experience')"></ion-icon>
-
-          <div :key="exp.id" v-for="exp in contents.experience">
-            <ion-icon name="remove-circle" class="text-red-600 text-xl cursor-pointer" @click="onDeleteForm('experience', exp.id)"></ion-icon>
-            <div className="form-control">
-              <label htmlFor='company'>Company</label>
-              <input :id="'company-' + exp.id"
-                     :data-id="exp.id" 
-                     data-input="company" 
-                     :value='exp.company' 
-                     type='text' 
-                     placeholder='Example Co' 
-                     @input="onInput($event, 'experience')" 
-                />
-            </div>
-
-            <div className="form-control">
-              <label htmlFor='position'>Position</label>
-              <input :id="'position-' + exp.id" 
-                     :data-id="exp.id" 
-                     data-input="position" 
-                     :value='exp.position' 
-                     type='text' 
-                     placeholder='Position' 
-                     @input="onInput($event, 'experience')" 
-                />
-            </div>
-
-            <div className="form-control">
-              <label htmlFor='company'>Description</label>
-              <textarea :id="'description-' + exp.id"
-                        :data-id="exp.id" 
-                        data-input="description" 
-                        :value='exp.description' 
-                        placeholder='A description of your job here' 
-                        @input="onInput($event, 'experience')" 
-                  />
-            </div>
-
-            <div className="form-control">
-              <label htmlFor='from_comp'>From</label>
-              <input :id="'from-comp-' + exp.id"
-                     :data-id="exp.id" 
-                     data-input="from_comp" 
-                     :value='exp.from_comp' 
-                     type='date' 
-                     @input="onInput($event, 'experience')" 
-                />
-            </div>
-
-            <div className="form-control">
-              <label htmlFor='to_comp'>To</label>
-              <input :id="'to-comp-' + exp.id"
-                     :data-id="exp.id"
-                     data-input="to_comp" 
-                     :value='exp.to_comp'  
-                     type='date' 
-                     @input="onInput($event, 'experience')" 
-                />
-            </div>
-          </div>
-        </div>
+    <div>
+      <div class="flex items-center justify-between">
+        <h4 className="section-title">Experience</h4>
+        <ion-icon name="caret-up" @click="onFormToggle($event, 'experience')" class="cursor-pointer hover:text-gray-600 text-2xl"></ion-icon>
       </div>
+      <ExperienceForm :contents="contents" 
+                      :toggles="toggles" 
+                      @update-contents="updateContents"  
+                      @delete-form="deleteForm" 
+                      @add-form="addForm" />
+    </div>
 
-      <input type='submit' value='Submit' className="bg-blue-500 text-white rounded-md py-1 px-2 hover:bg-blue-400 duration-500" />
-    </form>
+    <input type='submit' value='Submit' className="bg-blue-500 text-white rounded-md py-1 px-2 hover:bg-blue-400 duration-500" />
+  </form>
 </template>
 
 <script>
 import GeneralForm from './forms/GeneralForm.vue'
 import EducationForm from './forms/EducationForm.vue'
+import ExperienceForm from './forms/ExperienceForm.vue'
 
 export default {
   name: 'CvForm',
   components: {
     GeneralForm,
     EducationForm,
+    ExperienceForm,
   },
   props: {
     contents: Object
@@ -117,9 +60,6 @@ export default {
   methods: {
     onSubmit(e) {
       e.preventDefault
-    },
-    onInput(e, form){
-      this.$emit('update-contents', e, form)
     },
     updateContents(e, form){
       this.$emit('update-contents', e, form)
@@ -132,14 +72,8 @@ export default {
       e.target.name === 'caret-up' ? 
       e.target.name = 'caret-down' : e.target.name = 'caret-up'
     },
-    onAddForm(form){
-      this.$emit('add-form', form)
-    },
     addForm(form){
       this.$emit('add-form', form)
-    },
-    onDeleteForm(form, id){
-      this.$emit('delete-form', form, id)
     },
     deleteForm(form, id){
       this.$emit('delete-form', form, id)
---
+++ src/components/forms/ExperienceForm.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="experience-form" v-if="toggles['experience']">
+    <ion-icon name="add-circle" class="text-green-600 text-xl cursor-pointer" @click="onAddForm('experience')"></ion-icon>
+    <div :key="exp.id" v-for="exp in contents.experience">
+      <ion-icon name="remove-circle" class="text-red-600 text-xl cursor-pointer" @click="onDeleteForm('experience', exp.id)"></ion-icon>
+      <div className="form-control">
+        <label htmlFor='company'>Company</label>
+        <input :id="'company-' + exp.id"
+                :data-id="exp.id" 
+                data-input="company" 
+                :value='exp.company' 
+                type='text' 
+                placeholder='Example Co' 
+                @input="onInput($event, 'experience')" 
+            />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor='position'>Position</label>
+          <input :id="'position-' + exp.id" 
+                 :data-id="exp.id" 
+                 data-input="position" 
+                 :value='exp.position' 
+                 type='text' 
+                 placeholder='Position' 
+                 @input="onInput($event, 'experience')" 
+             />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor='company'>Description</label>
+          <textarea :id="'description-' + exp.id"
+                    :data-id="exp.id" 
+                    data-input="description" 
+                    :value='exp.description' 
+                    placeholder='A description of your job here' 
+                    @input="onInput($event, 'experience')" 
+             />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor='from_comp'>From</label>
+          <input :id="'from-comp-' + exp.id"
+                 :data-id="exp.id" 
+                 data-input="from_comp" 
+                 :value='exp.from_comp' 
+                 type='date' 
+                 @input="onInput($event, 'experience')" 
+             />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor='to_comp'>To</label>
+          <input :id="'to-comp-' + exp.id"
+                 :data-id="exp.id"
+                 data-input="to_comp" 
+                 :value='exp.to_comp'  
+                 type='date' 
+                 @input="onInput($event, 'experience')" 
+             />
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ExperienceForm',
+  props: {
+    contents: Object,
+    toggles: Object
+    },
+  methods: {
+    onInput(e, form){
+      this.$emit('update-contents', e, form)
+    },
+    onAddForm(form){
+      this.$emit('add-form', form)
+    },
+    onDeleteForm(form, id){
+      this.$emit('delete-form', form, id)
+    }
+  },
+}
+</script>
