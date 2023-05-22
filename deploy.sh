16b83b888c89f9aa226deeb76a11f9fea2828b69
supersakana <zacwilliamson@icloud.com>
2023-05-22 13:02:05 -0600

    added deeploy script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:supersakana.github.io/vue_resume.git main:gh-pages
+git push -f git push -f git@github.com:supersakana/vue_resume.git main:gh-pages
 
 cd -
