2bbe9a6258ee34c81cab0ef9e6446ce2b33e5dc5
supersakana <zacwilliamson@icloud.com>
2023-05-25 13:31:42 -0600

    deploy 3

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# git push -f git@github.com:supersakana/supersakana.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git push -f git@github.com:supersakana/vue_resume.git main:gh-pages
+git push -f git@github.com:supersakana/vue_resume.git main:gh-pages
 
 cd -
